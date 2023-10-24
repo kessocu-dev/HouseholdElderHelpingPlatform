da1c491fc8aea14bcfd1f25bf55ca1a4d0d43ac6
A3ter1sm <842384412@qq.com>
2023-10-24 17:55:57 +0800

    健康模块 Knife4j配置文件更新

---
+++ hehp-health/src/main/java/com/AkiTeam/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.AkiTeam.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+public OpenAPI springShopOpenAPI() {
+    return new OpenAPI()
+            .info(new Info().title("hehp-Health")
+                    .description("hehp-Health模块API文档")
+                    .version("v1")
+                    .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .externalDocs(new ExternalDocumentation()
+                    .description("后端文档")
+                    .url("https://springshop.wiki.github.org/docs"))
+            ;
+}
+}
